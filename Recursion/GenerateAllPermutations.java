4b21d8ab1e7919c6c5dfcede1aef2c8a8b279a73
saikumar-0193 <reddysai0193@gmail.com>
2025-07-07 23:21:44 +0530

    update

--- Recursion/GenerateAllPermutations.java
+++ Recursion/GenerateAllPermutations.java
@@ -3,17 +3,18 @@ import java.util.ArrayList;
 import java.util.List;
 public class GenerateAllPermutations {
         public static void main(String[] args) {
-            String str = "abc";
+            String str = "abc"; 
             List<String> result = new ArrayList<>();
             Helper(str,0, "", result);
+           System.out.println(result.get(1));
     }
     public static void Helper(String str, int index, String current, List<String>result) {
         if(index >= str.length()) {
-            System.out.println(current);
+            result.add(current);
             return;
         }
         for(int i = 0; i < index; i++) {
-
+        //
             Helper(str, i +1, current + str.charAt(i), result);
             Helper(str, index, current, result);
         }
--- Recursion/print.java
+++ Recursion/print.java
@@ -6,8 +6,8 @@ public class print {
     }
     public static void helper(int num) {
         if(num <= 0) return;
-         helper(num - 1);
+        
         System.out.print(num + " ");
-       
+        helper(num - 1);
     }
 }
